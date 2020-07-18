31aaa0df7009ae10ec115f58b4164eb6c916b48f
Silas Jo <67782634+ministar1126@users.noreply.github.com>
2020-07-18 15:11:05 +0200

    Update configs.js

--- src/constants/configs.js
+++ src/constants/configs.js
@@ -3,7 +3,7 @@ export default {
     apiURL: "http://kwikbookings.com/api",
     // baseURL: "http://192.168.100.52",
     // apiURL: "http://192.168.100.52/api",
-    google_map_key: 'AIzaSyCXU9mYBzIVbb3ljhHbwWj1IHAP373_RO4',
+    google_map_key: 'AIzaSyCXU9mY***************Athena*******************BzIVbb3ljhHbwWj1IHAP373_RO4',
     cloud_function_server_url: 'https://us-central1-taivla.cloudfunctions.net/',    
     latitude: 18.008841,
     longitude: -76.784286,
